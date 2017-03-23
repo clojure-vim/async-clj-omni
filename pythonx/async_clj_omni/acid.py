d8ce4c82561efb0c1d43be3bb7eea9f086f8632e
Dominic Monroe <dominic@juxt.pro>
2017-03-23 08:08:12 +0000

    Refactor acid completion into generic lib
    This makes porting to NCM easier

---
+++ pythonx/async_clj_omni/acid.py
@@ -0,0 +1,80 @@
+import threading
+from async_clj_omni.cider import cider_gather  # NOQA
+try:
+    from acid.nvim import localhost, path_to_ns
+    from acid.session import SessionHandler, send
+    loaded = True
+except:
+    loaded = False
+
+
+class Acid_nrepl:
+    def __init__(self, wc):
+        self.wc = wc
+
+    def send(self, msg):
+        self.wc.send(msg)
+
+    def watch(self, name, q, callback):
+        self.wc.watch(name, q, callback)
+
+    def unwatch(self, name):
+        self.wc.unwatch(name)
+
+
+class AcidManager:
+    def __init__(self, logger, vim):
+        self._vim = vim
+        self._logger = logger
+        self.__conns = {}
+
+    def on_init(self):
+        if loaded:
+            self.acid_sessions = SessionHandler()
+        else:
+            self._logger.debug('Acid.nvim not found. Please install it.')
+        self.sessions = {}
+
+    def get_wc(self, url):
+        return self.acid_sessions.get_or_create(url)
+
+    def get_session(self, url, wc):
+        if url in self.sessions:
+            return self.sessions[url]
+
+        session_event = threading.Event()
+
+        def clone_handler(msg, wc, key):
+            wc.unwatch(key)
+            self.sessions[url] = msg['new-session']
+            session_event.set()
+
+        wc.watch('dyn-session', {'new-session': None}, clone_handler)
+        wc.send({'op': 'clone'})
+        session_event.wait(0.5)
+
+        return self.sessions[url]
+
+    def gather_candidates(self, keyword):
+        if not loaded:
+            return []
+
+        address = localhost(self._vim)
+        if address is None:
+            return []
+        url = "nrepl://{}:{}".format(*address)
+        wc = self.get_wc(url)
+        session = self.get_session(url, wc)
+        ns = path_to_ns(self._vim)
+
+        def global_watch(cmsg, cwc, ckey):
+            self._logger.debug("Received message for {}".format(url))
+            self._logger.debug(cmsg)
+
+        wc.watch('global_watch', {}, global_watch)
+
+        return cider_gather(self._logger,
+                            Acid_nrepl(wc),
+                            keyword,
+                            session,
+                            ns)
--- rplugin/python3/deoplete/sources/acid.py
+++ rplugin/python3/deoplete/sources/acid.py
@@ -1,4 +1,4 @@
-import threading
+import deoplete.logger
 # Adds a git submodule to the import path
 import sys
 import os
@@ -6,29 +6,8 @@ basedir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(basedir, "../../acid"))
 sys.path.append(os.path.join(basedir, "../../../../pythonx/"))
 
-try:
-    from acid.nvim import localhost, path_to_ns
-    from acid.session import SessionHandler, send
-    loaded = True
-except:
-    loaded = False
-
-from async_clj_omni.cider import cider_gather  # NOQA
+from async_clj_omni.acid import Acid_nrepl, AcidManager
 from .base import Base  # NOQA
-
-
-class Acid_nrepl:
-    def __init__(self, wc):
-        self.wc = wc
-
-    def send(self, msg):
-        self.wc.send(msg)
-
-    def watch(self, name, q, callback):
-        self.wc.watch(name, q, callback)
-
-    def unwatch(self, name):
-        self.wc.unwatch(name)
 
 
 class Source(Base):
@@ -38,55 +17,11 @@ class Source(Base):
         self.mark = "[acid]"
         self.filetypes = ['clojure']
         self.rank = 200
-        self.__conns = {}
+        self._vim = vim
+        self._AcidManager = AcidManager(deoplete.logger.getLogger('acid_cider_completion_manager'), vim)
 
     def on_init(self, context):
-        if loaded:
-            self.acid_sessions = SessionHandler()
-        else:
-            self.debug('echomsg "Acid.nvim not found. Please install it."')
-        self.sessions = {}
-
-    def get_wc(self, url):
-        return self.acid_sessions.get_or_create(url)
-
-    def get_session(self, url, wc):
-        if url in self.sessions:
-            return self.sessions[url]
-
-        session_event = threading.Event()
-
-        def clone_handler(msg, wc, key):
-            wc.unwatch(key)
-            self.sessions[url] = msg['new-session']
-            session_event.set()
-
-        wc.watch('dyn-session', {'new-session': None}, clone_handler)
-        wc.send({'op': 'clone'})
-        session_event.wait(0.5)
-
-        return self.sessions[url]
+        self._AcidManager.on_init()
 
     def gather_candidates(self, context):
-        if not loaded:
-            return []
-
-        address = localhost(self.vim)
-        if address is None:
-            return []
-        url = "nrepl://{}:{}".format(*address)
-        wc = self.get_wc(url)
-        session = self.get_session(url, wc)
-        ns = path_to_ns(self.vim)
-
-        def global_watch(cmsg, cwc, ckey):
-            self.debug("Received message for {}".format(url))
-            self.debug(cmsg)
-
-        wc.watch('global_watch', {}, global_watch)
-
-        return cider_gather(self,
-                            Acid_nrepl(wc),
-                            context["complete_str"],
-                            session,
-                            ns)
+        return self._AcidManager.gather_candidates(context["complete_str"])
